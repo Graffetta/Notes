ad9f09de7cdaa339edeb0051fe90641a7a0232d9
Edoardo <48482307+Graffetta@users.noreply.github.com>
2019-11-25 23:26:40 +0100

    Update Notes.cpp

--- Notes.cpp
+++ Notes.cpp
@@ -6,79 +6,12 @@
 
 static Log Logger(Log::LevelTrace);
 
-void LoadNotes(std::string filename, std::vector<std::string>& notes)
-{
-	Logger.Verbose("Clearing existing notes.");
-	notes.clear();
-	Logger.Verbose("Notes cleared.");
-	std::ifstream InputStream(filename);
-	if (!InputStream.is_open()) 
-	{
-		Logger.Error("Could not open file.");
-		return;
-	}
-	Logger.Verbose("Loading notes.");
-	for (std::string line; getline(InputStream, line); )
-		notes.push_back(line);
-	Logger.Verbose("Done.");
-	InputStream.close();
-}
-
-void SaveNotes(std::string filename, std::vector<std::string>& notes)
-{
-	std::ofstream OutputStream(filename);
-	if (!OutputStream.is_open()) 
-	{
-		Logger.Error("Could not open file.");
-		return;
-	}
-	Logger.Verbose("Saving notes.");
-	for (const std::string Note : notes)
-		OutputStream << Note << '\n';
-	Logger.Verbose("Done.");
-	OutputStream.close();
-}
-
-void Print(std::vector<std::string>& notes)
-{
-	Logger.Verbose("Printing notes.");
-	unsigned int count = 0;
-	for (const std::string Note : notes)
-		printf(" <note #%u> %s\n", ++count, Note.c_str());
-	Logger.Verbose("Done.");
-}
-
-void CreateNote(std::vector<std::string>& notes)
-{
-	std::string UserInput;
-	printf("<notes - new> ");
-	getline(std::cin, UserInput, '\n');
-	notes.push_back(UserInput);
-}
-
-void DeleteNote(std::vector<std::string>& notes)
-{
-	Print(notes);
-	int Index;
-	bool ValidIndex = false;
-	while (!ValidIndex)
-	{
-		printf("insert index: ");
-		std::cin >> Index;
-		Index--;
-		if (Index == -1)
-			return;
-		if (Index <= notes.size() - 1 && Index >= 0)
-			ValidIndex = true;
-	}
-	notes.erase(notes.begin() + Index);
-}
-
-void ClearNotes(std::vector<std::string>& notes)
-{
-	Logger.Verbose("Clearing Notes.");
-	notes.clear();
-}
+void SaveNotes(std::string, std::vector<std::string>&);
+void LoadNotes(std::string, std::vector<std::string>&);
+void Print(std::vector<std::string>&);
+void CreateNote(std::vector<std::string>&);
+void DeleteNote(std::vector<std::string>&);
+void ClearNotes(std::vector<std::string>&);
 
 int main()
 {
@@ -98,7 +31,6 @@ int main()
 		else if (UserInput == "delete") { DeleteNote(Notes); }
 		else if (UserInput == "clear") { ClearNotes(Notes); }
 		else if (UserInput == "exit" || UserInput == "quit") { Exit = true; }
-		else if (UserInput == "-v") { Logger.SetLevel(Log::LevelVerbose); }
 		else { Logger.Warn("No such command found."); }
 	}
 	Logger.Trace("Goodbye.");
